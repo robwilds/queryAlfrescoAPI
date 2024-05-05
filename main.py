0b40fb821b9dc9cd01582f236de30a6a100d831f
Rob Wilds <robwilds@me.com>
2024-05-04 21:29:52 -0400

    all done..may add flask

--- main.py
+++ main.py
@@ -17,19 +17,6 @@ def pullGroups():
 
     print ('query url is: ' + groupQuery + '--->>>>')
     data=runQuery('get',groupQuery,'',auth)
-    #jdata = json.loads(data)
-    #print ('data -->>> ')
-
-    #now loop through the results and apply to panda dataframe
-    #for entry in data['list']['entries']:
-        
-        #name.append(entry['displayName'])
-        #id.append(entry['id'])
-        #tempdata = json.dumps(entry)
-
-        #print (entry)
-
-    #groupFrame = pd.DataFrame([name],[id])
 
     return data
 
@@ -48,44 +35,36 @@ def pullPeopleGroups(personid):
 def main():
     peopleID = []
     peopleStatus = []
-    groupID = []
-    groupDisplayName = []
     peopleGroupID = []
     peopleGroupName = []
+    peopleGroupStatus = []
 
     file_name = 'peopleGroups.xlsx'
-
-    #print (pullGroups()) #may not need this pull if merging people to groups
-    #print (pullPeople())
 
     for entry in pullPeople()['list']['entries']:
         #print (entry['entry']['id'] + ' ' + str(entry['entry']['enabled']))
 
-        #now load each user into a dataframe for People
+        #now load each user into array for user later
         peopleID.append(entry['entry']['id'])
         peopleStatus.append(entry['entry']['enabled'])
 
-    #need to input people into a dataframe
-    #peopleDF = pd.DataFrame([peopleID,peopleStatus]).T
-
-    #print(peopleDF) #Debug: display people DataFrame
-
     #now loop through each username and pass to pull people groups
-
-
     for person in peopleID:
 
         for item in pullPeopleGroups(person)['list']['entries']:
             #print (item) #debugging
             #print(person + ' - ' + item['entry']['id']) #debug
             peopleGroupID.append(person)
+            peopleGroupStatus.append(peopleStatus[list(peopleID).index(person)])
             peopleGroupName.append(item['entry']['id'])
 
     #now need to merge and output
-    peopleGroupDF = pd.DataFrame([peopleGroupID,peopleGroupName]).T
+    peopleGroupDF = pd.DataFrame([peopleGroupID,peopleGroupStatus,peopleGroupName]).T
 
     print (peopleGroupDF)
     peopleGroupDF.to_excel(file_name)
+    #peopleGroupDF.to_html(file_name+'.html')
+
     
 if __name__ == "__main__":
     main()
