c4f362c40d37f4647b39e372c0cf75a53425505e
Rob Wilds <robwilds@me.com>
2024-05-09 13:06:21 -0400

    fixed json data output

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import peopleGroups
 import pandas as pd
 
-from flask import Flask,json
+from flask import Flask,json,Response
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
@@ -9,4 +9,5 @@ CORS(app)
 
 @app.route("/peoplegroups")
 def peoplegroups():
-    return json.dumps(peopleGroups.main().to_json(orient = 'records'))
+    return Response(peopleGroups.main().to_json(orient = 'records'),mimetype='text/json')
+    #return json.dumps(peopleGroups.main().to_json(orient = 'records'))
