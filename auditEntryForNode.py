709c6cfc645448ec03a86963380a3e8b32ae040c
Rob Wilds <robwilds@me.com>
2024-05-13 01:10:52 -0400

    updated audit entry details

--- auditEntryForNode.py
+++ auditEntryForNode.py
@@ -12,8 +12,10 @@ auth = os.getenv("auth")
 nodeID = []
 appEntryID = []
 appEntryDetails = []
+appEntryDate = []
+appEntryUser = []
 
-cols = {0: 'auditEntryID',1:'nodeID',2:'details'}
+cols = {0: 'nodeID',1:'auditEntryID',2:'entryDate',3:'details',4:'user'}
 
 def pullAuditEntryForNode(nodeid):
 
@@ -30,24 +32,35 @@ def pullAuditEntryDetailsForNode(auditentryid):
 
     data=runQuery('get',auditEntryDetailsForNodeQuery,'',auth)
 
-    return data
+    #See if the actual action data can be returned
+    actionDetailsForNode = data['entry']['values']['/alfresco-access/transaction/action']
+    actionUserForNode = data['entry']['values']['/alfresco-access/transaction/user']
+
+    return [actionDetailsForNode,actionUserForNode]
 
 def main(nodeid):
     
     for entry in pullAuditEntryForNode(nodeid)['list']['entries']:
         #print(nodeid + '-' + str(entry['entry']['id'])) #debug
 
+        #set the entry details and user now so there's one call
+        entryDetails = pullAuditEntryDetailsForNode(entry['entry']['id'])[0]
+        entryUser = pullAuditEntryDetailsForNode(entry['entry']['id'])[1]
+
         nodeID.append(nodeid) #this will be the same nodeid for each audit entry id
         appEntryID.append(entry['entry']['id'])
-        appEntryDetails.append(pullAuditEntryDetailsForNode(entry['entry']['id']))
+        appEntryDate.append(entry['entry']['createdAt'])
+        appEntryDetails.append(entryDetails)
+        appEntryUser.append(entryUser)
     
-    auditentryfornodeDF = pd.DataFrame([appEntryID,nodeID,appEntryDetails]).T
+    auditentryfornodeDF = pd.DataFrame([nodeID,appEntryID,appEntryDate,appEntryDetails,appEntryUser]).T
     auditentryfornodeDF.rename(columns=cols,inplace=True)
 
     print (auditentryfornodeDF)
+    auditentryfornodeDF.to_excel('auditentryfornode.xlsx')
     return auditentryfornodeDF
 
 if __name__ == "__main__":
 
     #nodeid = '7a0eb8ca-8b69-43b4-8062-4b79cbddc750' #testing node on rwilds232
-    main('7a0eb8ca-8b69-43b4-8062-4b79cbddc750')
+    main('7a0eb8ca-8b69-43b4-8062-4b79cbddc750') #debug if this file is run directly not from flask app
