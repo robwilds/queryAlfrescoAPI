f26763428dcc09e3951f8b3169c16c5c281a96f4
rob wilds <robwilds@me.com>
2024-09-23 11:36:19 -0400

    updated baseline config for sending to elastic with way of refreshing data for chart

--- elastic/sendDatatoElastic.py
+++ elastic/sendDatatoElastic.py
@@ -1,48 +1,41 @@
 # Import necessary libraries
 import pandas as pd  # For data manipulation with DataFrames
-#from getpass import getpass  # For securely getting user input (passwords)
-from elasticsearch import Elasticsearch, helpers
+from elasticsearch import Elasticsearch
 import configparser
-import os
-from dotenv import load_dotenv
-
-elasticCloudID=os.getenv("elasticcloudid")
-elasticKey=os.getenv("elasticapikey")
-elasticUser = os.getenv("elasticuser")
-elasticPassword = os.getenv("elasticpassword")
 
 config = configparser.ConfigParser()
 config.read('elastic.ini')
 
 def main(elasticData = None,index=None,document=None):
-    # Read data from a CSV file into a DataFrame
-    # The Repair Cafe International dataset can be downloaded at https://openrepair.org/open-data/downloads/
-    #repair_data_df = pd.read_csv('/path/to/OpenRepairData_v0.3_RepairCafeInt_202309.csv')
 
     # Create an Elasticsearch client using the provided credentials
-    #es = Elasticsearch(cloud_id=elasticID, api_key=elasticKey)
+    #es = Elasticsearch(cloud_id=elasticID, api_key=elasticKey) #use this for api key access
     es = Elasticsearch(
     cloud_id=config['ELASTIC']['cloud_id'],
     http_auth=(config['ELASTIC']['user'], config['ELASTIC']['password']))
 
     print ("es info -> " + str(es.info()))
 
+    #clear the index(ces)
+    indices = ['lord-of-the-rings']
+    es.delete_by_query(index=indices, body={"query": {"match_all": {}}})
+
     #now index some data
-    es.index(
+    es.index(refresh='true',
  index='lord-of-the-rings',
  document={
   'character': 'Aragon',
   'quote': 'It is not this day.'
  })
     
-    es.index(
+    es.index(refresh='true',
  index='lord-of-the-rings',
  document={
   'character': 'Gandalf',
   'quote': 'A wizard is never late, nor is he early.'
  })
     
-    es.index(
+    es.index(refresh='true',
  index='lord-of-the-rings',
  document={
   'character': 'Frodo Baggins',
