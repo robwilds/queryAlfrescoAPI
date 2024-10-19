1edc6d2d08604c5ca53b301b0d3b694d1b3ec48f
Rob Wilds <robwilds@me.com>
2024-10-19 18:30:01 -0400

    added Llama test python routine

---
+++ llama/llamaChat.py
@@ -0,0 +1,49 @@
+from llama_cpp import Llama
+from llama_cpp_agent import LlamaCppAgent, MessagesFormatterType
+from llama_cpp_agent.providers import LlamaCppPythonProvider
+from llama_cpp_agent.chat_history import BasicChatHistory
+from llama_cpp_agent.chat_history.messages import Roles
+
+SYSTEM_PROMPT = "You are a useful chatbot" #Customize system prompt to fit your need
+CHAT_TEMPLATE = MessagesFormatterType.LLAMA_3  #Prompt format to use
+MODEL_PATH = "Llama-3.2-1B-Instruct-Q4_K_M.gguf" #Llama-3.2-1B-Instruct-Q4_K_M.gguf used for chat bot style interaction https://huggingface.co/bartowski/Llama-3.2-1B-Instruct-GGUF/resolve/main/Llama-3.2-1B-Instruct-Q4_K_M.gguf
+TEMPERATURE=0.3  #For small models, low temperature is often better
+MAX_NEW_TOKENS = 1024  #Max tokens to output
+CONTEXT_WINDOW=8000 #Max context window. Up to 128k. The higher the value, the more GPU memory you will need
+N_GPU_LAYERS=-1  #Number of layers to put on GPU. -1 = all
+N_BATCH=1024  #Increase if long prompts and you need faster inference.
+
+llm = Llama(
+    model_path=MODEL_PATH,
+    n_gpu_layers=N_GPU_LAYERS,
+    n_batch=N_BATCH,
+    n_ctx=CONTEXT_WINDOW
+
+)
+
+provider = LlamaCppPythonProvider(llm)
+settings = provider.get_provider_default_settings()
+settings.temperature = TEMPERATURE
+settings.max_tokens = MAX_NEW_TOKENS
+settings.stream = True
+
+agent = LlamaCppAgent(
+    provider,
+    system_prompt=SYSTEM_PROMPT,
+    predefined_messages_formatter_type=CHAT_TEMPLATE,
+    debug_output=False
+)
+
+prompt = "who was the best president"
+
+response = agent.get_chat_response(
+    prompt,
+    llm_sampling_settings=settings,
+    #chat_history=chat_history, Not useful here but to be used if you want to keep track of previous prompts
+    returns_streaming_generator=True,
+    print_output=False
+)
+
+
+for chunk in response:
+    print(chunk)
---
+++ llama/readme.md
@@ -0,0 +1,4 @@
+# Models
+
+for chat, be sure to download https://huggingface.co/bartowski/Llama-3.2-1B-Instruct-GGUF/resolve/main/Llama-3.2-1B-Instruct-Q4_K_M.gguf
+
